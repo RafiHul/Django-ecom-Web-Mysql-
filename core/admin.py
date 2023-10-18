806aaf1364558ee2cf2c8f598051ca0db1218aa5
RafiHul <rafihul2905@gmail.com>
2023-10-18 09:06:44 +0700

    move inspect db to models.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Username, Produk, Keranjang
 # Register your models here.
+
+admin.site.register(Username)
+admin.site.register(Produk)
+admin.site.register(Keranjang)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Keranjang(models.Model):
+    id_keranjang = models.OneToOneField('Produk', models.DO_NOTHING, db_column='id_keranjang', primary_key=True)
+    jumlah = models.PositiveIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'keranjang'
+
+class Produk(models.Model):
+    nama_barang = models.CharField(max_length=100)
+    gambar = models.TextField()
+    harga = models.PositiveIntegerField()
+    jumlah = models.PositiveIntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'produk'
+
+    def __str__(self):
+        return self.nama_barang
+
+class Username(models.Model):
+    username_akun = models.CharField(primary_key=True, max_length=100)
+    password_akun = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'username'
