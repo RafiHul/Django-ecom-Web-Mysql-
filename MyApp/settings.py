f245419e855d5444395f7e93d80d7bb8ab79eea1
RafiHul <rafihul2905@gmail.com>
2023-10-18 07:43:48 +0700

    add database settings

--- MyApp/settings.py
+++ MyApp/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'MyApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'kasir_database',
+		'USER': 'kasir',
+		'PASSWORD': 'yes',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
+
 
 
 # Password validation
