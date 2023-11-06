776dfc8e8cc7f488d676a760e05d82e617f647f7
RafiHul <rafihul2905@gmail.com>
2023-11-06 20:21:10 +0700

    some error in checkout

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
 from core.models import Produk
-
-
 from .cart import Cart
 
 @login_required
@@ -14,8 +13,10 @@ def cart_view(request):
     })
 
 @login_required
-def add_pk(request,pk,price):
-    request.session['selected_product'] = {"pk":pk,'price':price}
+def add_pk(request,pk,price,quantity):
+    quantity = request.POST
+    print(pk,price,quantity)
+    request.session['selected_product'] = {"pk":pk,'price':price,'quantity':quantity,'total':int(price) * int(quantity)}
     return redirect('cart:checkout')
 
 @login_required
@@ -25,13 +26,13 @@ def add_checkout(request):
     if selected_product:
         pk = selected_product.get('pk')
         price = selected_product.get('price')
-
-        cart = Cart(request)
-        cart.add(produk_id=pk,price=price)
+        quantity = selected_product.get('quantity')
+        total = selected_product.get('total')
         
         messages.success(request, 'Produk berhasil ditambahkan ke keranjang.')
     return render(request, 'cart/checkout.html',{
-        'cart': cart,
+        'price': price,
+        'total': total,
     })
 
 @login_required
--- items/views.py
+++ items/views.py
@@ -1,14 +1,29 @@
 from core.models import Produk
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+
 from .forms import NewItem, EditItem
+from cart.forms import QuantityForm
+from cart.views import add_pk
 
 def detail(request,pk):
     produk = get_object_or_404(Produk,pk=pk)
     q = produk.created_by
+
+    if request.method == "POST":
+        form = QuantityForm(request.POST)
+
+        if form.is_valid():
+            quantity2 = request.POST.get('jumlah')
+            quantity = int(quantity2)
+            return add_pk(request,pk=pk,price=produk.harga,quantity=quantity)
+    else:
+        form = QuantityForm()
+
     return render(request, 'items/detail.html',{
         'produk': produk,
-        'q':q
+        'q':q,
+        'form':form,
     })
 
 @login_required
